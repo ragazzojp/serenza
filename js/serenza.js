3c383253492811fb04c2668d52f5abf8d3ac91ed
Stefano Pettini <stefano@pettini.eu>
2018-05-08 22:21:36 +0200

    Added UI translations

--- js/serenza.js
+++ js/serenza.js
@@ -8,6 +8,82 @@ var timeout = null;
 // The word lists
 const words = { };
 
+// UI translations
+const translations = {
+    it: {
+        lang_legend: "Lingua",
+        len_legend: "Lunghezza delle parole",
+        len_short: "Corta",
+        len_medium: "Media",
+        len_long: "Lunga",
+        len_verylong: "Molto lunga",
+        time_legend: "Tempo di visualizzazione di ogni parola",
+        time_short: "Corto",
+        time_medium: "Medio",
+        time_long: "Lungo",
+        duration_legend: "Durata dell'esercizio",
+        duration_5min: "5 minuti",
+        duration_10min: "10 minuti",
+        duration_15min: "15 minuti",
+        play: "Play",
+        stop: "Stop"
+    },
+    en: {
+        lang_legend: "Language",
+        len_legend: "Length of the words",
+        len_short: "Short",
+        len_medium: "Medium",
+        len_long: "Long",
+        len_verylong: "Very long",
+        time_legend: "Amount of time each word is displayed",
+        time_short: "Short",
+        time_medium: "Medium",
+        time_long: "Long",
+        duration_legend: "Duration of the exercise",
+        duration_5min: "5 minutes",
+        duration_10min: "10 minutes",
+        duration_15min: "15 minutes",
+        play: "Play",
+        stop: "Stop"
+    },
+    fr: {
+        lang_legend: "Langue",
+        len_legend: "Longueur des mots",
+        len_short: "Courte",
+        len_medium: "Moyenne",
+        len_long: "Longue",
+        len_verylong: "Très longue",
+        time_legend: "Temps d'affichage de chaque mot",
+        time_short: "Court",
+        time_medium: "Moyen",
+        time_long: "Long",
+        duration_legend: "Durée de l'exercice",
+        duration_5min: "5 minutes",
+        duration_10min: "10 minutes",
+        duration_15min: "15 minutes",
+        play: "Lecture",
+        stop: "Arrêt"
+    },
+    de: {
+        lang_legend: "Sprache",
+        len_legend: "Wörterlänge",
+        len_short: "Kurze",
+        len_medium: "Durchschnittliche",
+        len_long: "Lange",
+        len_verylong: "Sehr lange",
+        time_legend: "Anzeigezeit jedes Wortes",
+        time_short: "Kurze",
+        time_medium: "Durchschnittliche",
+        time_long: "Lange",
+        duration_legend: "Dauer der Übung",
+        duration_5min: "5 Minuten",
+        duration_10min: "10 Minuten",
+        duration_15min: "15 Minuten",
+        play: "Wiedergabe",
+        stop: "Stopp"
+    }
+}
+
 function shuffle(a) {
     let j, x, i;
     for (i = a.length - 1; i > 0; i--) {
@@ -15,6 +91,22 @@ function shuffle(a) {
         x = a[i];
         a[i] = a[j];
         a[j] = x;
+    }
+}
+
+function applyTranslation() {
+
+    // Parse the settings
+    const settings = parseSettings();
+
+    console.log("Applying translation, language:", settings.lang);
+
+    // The translation to use
+    const trans = translations[settings.lang];
+
+    // Apply the texts
+    for (const id in trans) {
+        $("#" + id).text(trans[id]);
     }
 }
 
@@ -159,8 +251,12 @@ window.onload = function() {
     loadWordList("en", "words/en.json");
 
     // Setup event handlers
+    $("#settings input[name=lang]").click(applyTranslation);
     $("#play").click(play);
     $("#stop").click(stop);
+
+    // Apply the initial translation
+    applyTranslation();
 
     // Apply the initial state
     applyState();
